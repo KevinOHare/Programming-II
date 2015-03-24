7ef98521f84d3c2b524fd646f75189d1801eb2ff
JLeathem93 <jonnieleathem@hotmail.co.uk>
2015-03-24 15:49:33 +0000

    Treatment Room

--- PAS/PAS/src/NHSsystem/TreatmentRoom.java
+++ PAS/PAS/src/NHSsystem/TreatmentRoom.java
@@ -4,16 +4,84 @@
 package NHSsystem;
 
 /**
+ * Class that represents the treatment rooms
+ * 
  * @author chrismcclune
  *
  */
 public class TreatmentRoom {
 
 	/**
-	 * 
+	 * The treatment room number
+	 */
+	private int roomNumber;
+
+	/**
+	 * The availability of the treatment room
+	 */
+	private boolean isAvailable;
+
+	/**
+	 * How long a patient has been in the room
+	 */
+	private void timer() {
+
+	}
+
+	/**
+	 * Default constructor
 	 */
 	public TreatmentRoom() {
-		// TODO Auto-generated constructor stub
+
+	}
+
+	/**
+	 * Args based constructor
+	 * 
+	 * @param roomNumber
+	 * @param isAvailable
+	 */
+	public TreatmentRoom(int roomNumber, boolean isAvailable) {
+		this.roomNumber = roomNumber;
+		this.isAvailable = isAvailable;
+	}
+
+	/**
+	 * Gets the room number
+	 * 
+	 * @return the roomNumber
+	 */
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	/**
+	 * Sets the room number
+	 * 
+	 * @param roomNumber
+	 *            the roomNumber to set
+	 */
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	/**
+	 * Gets the availability of the room
+	 * 
+	 * @return the isAvailable
+	 */
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	/**
+	 * Sets the availability of the room
+	 * 
+	 * @param isAvailable
+	 *            the isAvailable to set
+	 */
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
 	}
 
 }
