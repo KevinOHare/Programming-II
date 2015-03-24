4ee22ef41f512234487ee2a41510219b911c8de0
chrismcclune <chrismcclune@Chriss-MacBook-Air.local>
2015-03-24 16:25:36 +0000

    update to jar

--- PAS/PAS/src/javafx/Main.java
+++ PAS/PAS/src/javafx/Main.java
@@ -21,8 +21,8 @@ public class Main extends Application {
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			//primaryStage.setScene(scene);
 			//primaryStage.show();
-			   Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));		  
-		        Scene scene = new Scene(root);  
+			   Parent root = FXMLLoader.load(getClass().getResource("Triage.fxml"));		  
+		        Scene scene = new Scene(root, 400, 300);  
 		        stage.setTitle("PAS Login");
 		        stage.setScene(scene);
 		        stage.show();
