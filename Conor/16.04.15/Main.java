bd3288b6f516d5b14e4f4d0ff482c8f6ed159227
mcaleavey <conormca12345@hotmail.co.uk>
2015-04-16 14:34:58 +0100

    login_files

---
+++ Conor/16.04.15/Main.java
@@ -0,0 +1,59 @@
+package javafx;
+
+import java.awt.Button;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	
+	
+	 @Override
+	    public void start(Stage primaryStage) {
+
+	        Stage anotherStage = new Stage();
+
+	        try {
+	            FXMLLoader loader = new FXMLLoader(); // FXML for primary stage
+	            Parent root = FXMLLoader.load(getClass().getResource(
+						"Login.fxml"));
+	            Scene scene = new Scene(root);
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+
+	            /**
+	             * 
+	            FXMLLoader anotherLoader = new FXMLLoader() ; // FXML for second stage
+	             
+	            Parent anotherRoot = FXMLLoader.load(getClass().getResource(
+						"Queue.fxml"));
+	            Scene anotherScene = new Scene(anotherRoot);
+	            anotherStage.setScene(anotherScene);
+	            anotherStage.show();
+				*/
+	        } catch (Exception exc) {
+
+	            exc.printStackTrace();
+
+	        }
+	    }
+
+	
+       
+
+   
+	
+
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+}
